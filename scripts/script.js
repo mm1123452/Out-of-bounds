5d45458b76395e9bfa2907f668604acd98425065
Avisa <manelis1102@gmail.com>
2020-06-18 10:06:39 +0300

    feature: add donations and footer sections

--- scripts/script.js
+++ scripts/script.js
@@ -1,8 +1,17 @@
 import { Carousel } from "./Carousel.js";
 
-const carouselList = document.querySelectorAll('.carousel');
+const carouselList = document.querySelectorAll(".carousel");
+const donateBtn = document.querySelector(".donations__submit-button");
 
 carouselList.forEach((carouselElement) => {
   const carousel = new Carousel(carouselElement);
   carousel.generateCarousel();
 });
+
+donateBtn.addEventListener("click", (e) => {
+  e.preventDefault();
+  e.target.textContent = "Thank You!";
+  e.target.parentNode.reset();
+});
+
+
