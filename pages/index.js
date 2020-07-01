14835c6d0d2cf548823e7bc3f17253dc2c968a2f
Tanya Margulis <Tany999m@gmail.com>
2020-07-01 23:51:42 +0200

    feature: reactive carousel and js cosmetic changes

--- pages/index.js
+++ pages/index.js
@@ -1,7 +1,11 @@
-import { Carousel } from "./Carousel.js";
+import { Carousel } from "../components/Carousel.js";
+import {
+  carouselList,
+  donateBtn
+} from "../utils/constants.js"
 
-const carouselList = document.querySelectorAll(".carousel");
-const donateBtn = document.querySelector(".donations__submit-button");
+// const carouselList = document.querySelectorAll(".carousel");
+// const donateBtn = document.querySelector(".donations__submit-button");
 
 carouselList.forEach((carouselElement) => {
   const carousel = new Carousel(carouselElement);
---
+++ utils/constants.js
@@ -0,0 +1,6 @@
+const carouselList = document.querySelectorAll(".carousel");
+const donateBtn = document.querySelector(".donations__submit-button");
+
+export { carouselList,
+         donateBtn
+}
