eb06b61c98081ef21a1f0e5c7f50dbfb623d09c3
Mabel <macmota@hotmail.com>
2020-07-26 11:08:21 -0400

    fix: switch to single quotes

--- src/utils/constants.js
+++ src/utils/constants.js
@@ -4,7 +4,7 @@ const formElement = document.querySelector('.donations__form');
 const nameInput = formElement.elements.name
 const surnameInput = formElement.elements.surname
 const emailInput = formElement.elements.email
-const donationAmount = formElement.elements["donation-amount"]
+const donationAmount = formElement.elements['donation-amount']
 const selectCheckRadioValue = (radioArray) => {
    const selected = radioArray.find(item => item.checked)
    return selected.value
@@ -17,9 +17,9 @@ const settingsForValidation = {
   errorClass: 'donations__input-error_type_active',
 };
 
-const baseUrl = "https://api.outofbound.tk"
+const baseUrl = 'https://api.outofbound.tk'
 
-const headers = {"Content-Type": "application/json"}
+const headers = {'Content-Type': 'application/json'}
 
 export { carouselList, donateBtn, formElement, settingsForValidation,
   baseUrl, headers, nameInput, surnameInput, emailInput, donationAmount, selectCheckRadioValue  };
